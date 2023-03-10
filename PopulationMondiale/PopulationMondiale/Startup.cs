a1f4f1bd13afd4cd425855f095a833f02568ef0d
el-tegy <67525407+el-tegy@users.noreply.github.com>
2023-03-11 00:07:33 +0100

    Update Startup.cs

--- PopulationMondiale/PopulationMondiale/Startup.cs
+++ PopulationMondiale/PopulationMondiale/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
-using DotNetCoreSqlDb.Models;
+using PopulationMondiale.Models;
 
 namespace PopulationMondiale
 {

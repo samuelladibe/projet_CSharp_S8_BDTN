0eca3fa25019f9d77796186a6e06386267ef2513
samuelladibe <103931828+samuelladibe@users.noreply.github.com>
2023-03-11 15:14:29 +0100

    Update Startup.cs

--- PopulationMondiale/PopulationMondiale/Startup.cs
+++ PopulationMondiale/PopulationMondiale/Startup.cs
@@ -78,7 +78,7 @@ namespace PopulationMondiale
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "PopulationMondiale API V1");
                 c.RoutePrefix = "swagger/ui";
                 c.DocExpansion(DocExpansion.List);
             });

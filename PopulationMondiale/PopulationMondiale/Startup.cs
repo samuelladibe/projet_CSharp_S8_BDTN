81b3361d42022f46cdafeed49de8444e6d5fb223
el-tegy <67525407+el-tegy@users.noreply.github.com>
2023-03-11 14:01:40 +0100

    Add a new assembly 'Microsoft.OpenApi.Models'

--- PopulationMondiale/PopulationMondiale/Startup.cs
+++ PopulationMondiale/PopulationMondiale/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using PopulationMondiale.Data;
 using PopulationMondiale.Models;
 

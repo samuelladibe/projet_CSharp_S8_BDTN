542541eaafd06a42b8971b7ff2cbe9d5ff772b0d
ladibe samuel <ladibesamuel@gmail.com>
2023-03-13 16:37:02 +0100

    Add units tests

---
+++ PopulationMondiale/PopulationMondiale.Tests/PopulationMondiale_IsContinent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PopulationMondiale.Models;
+
+namespace PopulationMondiale.Tests
+{
+     public class PopulationMondiale_IsContinent
+    {
+        [Fact]
+        public void IsContinent_ReturnTrue()
+        {
+            var continent = new Continent
+            {
+                NomContinent = "Europe",
+                Pays_ = new List<Pays>()
+            };
+            bool result = continent.NomContinent.Equals("Europe");
+            
+            Assert.True(result);
+        }
+    }
+}
---
+++ PopulationMondiale/PopulationMondiale.Tests/PopulationMondiale_IsPays.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PopulationMondiale.Models;
+
+namespace PopulationMondiale.Tests
+{
+    public class PopulationMondiale_IsPays
+    {
+        [Fact]
+        public void IsPays_ReturnTrue()
+        {
+            var pays = new Pays
+            {
+                NomPays = "UK",
+                ContinentId = 0,
+                Population_ = new List<Population>()
+            };
+            bool result = pays.NomPays.Equals("UK");
+
+            Assert.True(result);
+        }
+    }
+}
